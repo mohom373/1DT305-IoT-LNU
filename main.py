67d964a63264449540c534058de3b3acfd6014ac
Mohammad Mahir Omar <mohom373@student.liu.se>
2024-06-10 12:33:50 +0200

    Temperture sensor example code

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 # main.py -- put your code here!
+import machine
 import time
-from machine import Pin
 
-# Set the OUTPUT pin to on-board LED
-led = Pin("LED", Pin.OUT)
+adc = machine.ADC(27)
 
-# Runs forever
-while True:
-  led.on()
-  time.sleep(0.2)
-  led.off()
-  time.sleep(1.0)
+sf = 4095/65535 # Scale factor
+volt_per_adc = (3.3 / 4095)
+
+while True: 
+  time.sleep(2)
+  millivolts = adc.read_u16()
+
+  adc_12b = millivolts * sf
+
+  volt = adc_12b * volt_per_adc
+
+  # MCP9700 characteristics
+  dx = abs(50 - 0)
+  dy = abs(0 - 0.5)
+
+  shift = volt - 0.5
+
+  temp = shift / (dy / dx)
+  print("temp = ", temp)
+  #print("mv = ", millivolts)
+  #print("volt = ", volt)
+  time.sleep(1)
