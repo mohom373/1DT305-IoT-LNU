20cff7cd7814417c3cc649fdd148004d0a335ea4
Mohammad Mahir Omar <mohom373@student.liu.se>
2024-06-11 00:09:44 +0200

    Add Ubidots REST API tutorial

--- boot.py
+++ boot.py
@@ -1,8 +1,14 @@
 # boot.py -- run on boot-up
-import keys
+from keys import WIFI_SSID, WIFI_PASS, TOKEN
 import network
+
+import machine
+import urequests as requests
+import random
+
 from time import sleep
 
+# Function for WiFi connection
 def connect():
     wlan = network.WLAN(network.STA_IF)         # Put modem on Station mode
     if not wlan.isconnected():                  # Check if already connected
@@ -10,7 +16,7 @@ def connect():
         wlan.active(True)                       # Activate network interface
         # set power mode to get WiFi power-saving off (if needed)
         wlan.config(pm = 0xa11140)
-        wlan.connect(keys.WIFI_SSID, keys.WIFI_PASS)  # Your WiFi Credential
+        wlan.connect(WIFI_SSID, WIFI_PASS)  # Your WiFi Credential
         print('Waiting for connection...', end='')
         # Check if it is connected otherwise wait
         while not wlan.isconnected() and wlan.status() >= 0:
@@ -35,14 +41,43 @@ def http_get(url = 'http://detectportal.firefox.com/'):
     print(rec_bytes.decode("utf-8"))        # Print the response
     s.close()                               # Close connection
 
+# Builds the json to send the request
+def build_json(variable, value):
+    try:
+        data = {variable: {"value": value}}
+        return data
+    except:
+        return None
+
+# Random number generator
+def random_integer(upper_bound):
+    return random.getrandbits(32) % upper_bound
+
+# Sending data to Ubidots Restful Webserice
+def sendData(device, variable, value):
+    try:
+        url = "https://industrial.api.ubidots.com/"
+        url = url + "api/v1.6/devices/" + device
+        headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
+        data = build_json(variable, value)
+
+        if data is not None:
+            print(data)
+            req = requests.post(url=url, headers=headers, json=data)
+            return req.json()
+        else:
+            pass
+    except:
+        pass
+
 # WiFi Connection
 try:
     ip = connect()
 except KeyboardInterrupt:
     print("Keyboard interrupt")
 
-# HTTP request
+""" # HTTP request
 try:
     http_get()
 except (Exception, KeyboardInterrupt) as err:
-    print("No Internet", err)
+    print("No Internet", err) """
--- main.py
+++ main.py
@@ -14,3 +14,16 @@ while True:
     except Exception as error:
         print("Exception occurred", error)
     time.sleep(2) """
+
+from keys import DEVICE_LABEL, VARIABLE_LABEL
+
+from boot import random_integer, sendData
+from time import sleep
+
+DELAY = 5
+
+# Your device send a random value between 0 and 100 every five second to Ubidots
+while True:
+    value = random_integer(100)
+    returnValue = sendData(DEVICE_LABEL, VARIABLE_LABEL, value)
+    sleep(DELAY)
